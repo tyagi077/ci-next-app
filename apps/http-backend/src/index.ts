f5b2c4501f72f04b8cb7d9ba8ff3e0e12b5f116c
Mayank Tyagi <tyagimayank0501@gmail.com>
2025-03-26 03:09:57 +0530

    completed the app v1

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express"
+import {client} from "@repo/db/Client"
+const app = express()
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("Hi there");
+})
+app.post("/signup",async (req,res)=>{
+    const username = req.body.username;
+    const password =req.body.password;
+
+    const User=await client.user.create({
+        data:{
+            username,
+            password
+        }
+    })
+    res.json({
+        message:"Signup successfull",
+        id:User.id
+    })
+})
+
+app.listen(3000);
---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,15 @@
+import { WebSocket, WebSocketServer } from "ws";
+import {client} from "@repo/db/Client"
+const server = new WebSocketServer({
+    port:3001
+})
+
+server.on("Connection",async (socket)=>{
+   await client.user.create({
+        data:{
+            username:Math.random().toString(),
+            password:Math.random().toString()
+        }
+    })
+    socket.send("Hi there")
+})
